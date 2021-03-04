e3be27037710436b209bade92915f996b03c9149
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-03-04 11:13:27 +0100

    Create TempsAdditionnel.cs
    Ajout de la classe TempsAdditionnel.cs, classe permettant l'affichage d'un temps additionnel lors des matchs de type pas à pas.

---
+++ TempsAdditionnel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _22FIFA
+{
+    class TempsAdditionnel
+    {
+        public TempsAdditionnel (int tps_ad)
+        {
+            Console.WriteLine("TEMPS ADDITIONNEL - ");
+            Console.WriteLine("     " + tps_ad + " minutes minimum"); // Affichage du temps additionnel de seconde mi-temps
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
