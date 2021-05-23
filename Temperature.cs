800ad5b209e0e6e1ec04db38559c686a982acf61
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-05-23 12:57:50 +0200

    Create Temperature.cs
    Ajout du fichier Temperature.cs (fichier permettant un affichage de la température en fonction de la météo du match).

---
+++ Temperature.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _22FIFA
+{
+    class Temperature
+    {
+        public Temperature(int temperature)
+        {
+            Console.WriteLine(" TEMPERATURE EXTERIEURE : " + temperature + " °C"); // Affiche la température prévue au coup d'envoi du match
+            Console.ReadLine();
+        }
+    }
+}
