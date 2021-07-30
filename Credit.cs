4cf942bba633c9fc4f70032f2c5ee01d8227f8cf
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-07-30 20:29:31 +0200

    Create Credit.cs
    Ajout du fichier de crédit (Credit.cs), permettant l'affichage automatique des crédits lors de chaque fin de jeu.

---
+++ Credit.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace _22FIFA
+{
+    class Credit
+    {
+        public Credit(string test)
+        {
+            Console.WriteLine("CREDITS 22FIFA :");
+            Console.ReadLine();
+            Console.WriteLine("V.21.051 - 24/05/2021, 18:07 (UTC+01)"); // Crédit de fin de programme avec la dernière date et heure de modification manuellement
+            Console.ReadLine();
+            Console.WriteLine("Me contacter : ");
+            Console.WriteLine("○ GitHub : Guillaume-dm"); // Nom d'utilisateur Github du créateur
+            Console.WriteLine("○ [...]"); // Sous-entend qu'il y a d'autre moyens de communications non indiqués ici.
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
