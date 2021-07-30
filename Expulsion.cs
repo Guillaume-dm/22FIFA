dcf2143c2fe5a3b4f466bb5e947418d910a42786
Guillaume-dm <56759820+Guillaume-dm@users.noreply.github.com>
2021-07-30 20:27:57 +0200

    Create Expulsion.cs
    Ajout du fichier Expulsion.cs permettant l'affichage des expulsions lors des matchs de type pas à pas.

---
+++ Expulsion.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _22FIFA
+{
+    class Expulsion
+    {
+        public Expulsion(string name_red, string Equipe)
+        {
+            Console.WriteLine("CARTON ROUGE - " + Equipe);
+            Console.WriteLine("  " + name_red);
+        }
+    }
+}
